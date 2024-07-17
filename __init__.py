82d0fdd97b838eec93cca46ae51df375a0db6eb0
小萌新 <3209970865@qq.com>
2024-07-17 11:40:08 +0800

    Feat: add bl_info

--- __init__.py
+++ __init__.py
@@ -2,6 +2,17 @@ import bpy
 
 from . import key, ops
 
+bl_info = {
+    "name": "Hdr Rotation",
+    "description": "Rotation HDR by Shift+Right Drag in 3D View",
+    "author": "AIGODLIKE Community(小萌新)",
+    "version": (1, 0, 4),
+    "blender": (4, 2, 0),
+    "location": "3D View",
+    "support": "COMMUNITY",
+    "category": "幻之境",
+}
+
 
 class RotationPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
