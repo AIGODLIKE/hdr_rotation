0ba12846fc008be22400f53c7db1c902fd7b1229
小萌新 <3209970865@qq.com>
2024-11-21 15:14:45 +0800

    Feat: update preferences

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+import ast
+import re
+
 import bpy
 
 from . import key, ops
@@ -17,14 +20,52 @@ bl_info = {
 class RotationPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
 
+    def draw(self, context):
+        from .key import draw_keymap
+        column = self.layout.column()
+        for text in ["In 3D viewport (both renderd or material preview mode)",
+                     "Shift + Right-Drag on empty space",
+                     "you can rotate the enviroment texture to preview shaders and lighting more practical and faster",
+                     "When dragging on objects, moves 3D cursor"
+                     ]:
+            column.label(text=text)
+        column.separator()
+        column.label(text="Keymap:")
+        draw_keymap(column)
+
+
+def get_language_list() -> list:
+    """
+    Traceback (most recent call last):
+  File "<blender_console>", line 1, in <module>
+TypeError: bpy_struct: item.attr = val: enum "a" not found in ('DEFAULT', 'en_US', 'es', 'ja_JP', 'sk_SK', 'vi_VN', 'zh_HANS', 'ar_EG', 'de_DE', 'fr_FR', 'it_IT', 'ko_KR', 'pt_BR', 'pt_PT', 'ru_RU', 'uk_UA', 'zh_TW', 'ab', 'ca_AD', 'cs_CZ', 'eo', 'eu_EU', 'fa_IR', 'ha', 'he_IL', 'hi_IN', 'hr_HR', 'hu_HU', 'id_ID', 'ky_KG', 'nl_NL', 'pl_PL', 'sr_RS', 'sr_RS@latin', 'sv_SE', 'th_TH', 'tr_TR')
+    """
+    try:
+        bpy.context.preferences.view.language = ""
+    except TypeError as e:
+        matches = re.findall(r'\(([^()]*)\)', e.args[-1])
+        return ast.literal_eval(f"({matches[-1]})")
+
 
 def register():
     ops.register()
     key.register()
     bpy.utils.register_class(RotationPreferences)
+    language = "ZH_CN" if "ZH_CN" in get_language_list() else "zh_HANS"
+    bpy.app.translations.register("hdr_rotation", {language: {
+        ("*", "HDR Rotation"): "HDR旋转",
+        ("*", "In 3D viewport (both renderd or material preview mode)"): "在3D视图(渲染或材质预览模式)",
+        ("*", "Shift + Right-Drag on empty space"): "Shift+右键拖动 空白处",
+        ("*", "you can rotate the enviroment texture to preview shaders and lighting more practical and faster",
+         ): "您可以旋转环境纹理，以便更实用、更快速地预览着色器和照明效果",
+        ("*",
+         "When dragging on objects, moves 3D cursor",): "在对象上拖动时,会移动3D游标",
+        ("*", "Keymap:"): "快捷键:",
+    }})
 
 
 def unregister():
     ops.unregister()
     key.unregister()
     bpy.utils.unregister_class(RotationPreferences)
+    bpy.app.translations.unregister("hdr_rotation")
--- key.py
+++ key.py
@@ -1,24 +1,86 @@
 import bpy
 
-from .ops import HdrRotationOperator
+keymaps = []
 
-kc = bpy.context.window_manager.keyconfigs.addon  # 获取按键配置addon的
-km = kc.keymaps.new(name='3D View', space_type='VIEW_3D', region_type='WINDOW')
 
-kmi = None
+def get_kmi_operator_properties(kmi: 'bpy.types.KeyMapItem') -> dict:
+    """获取kmi操作符的属性
+    """
+    properties = kmi.properties
+    prop_keys = dict(properties.items()).keys()
+    dictionary = {i: getattr(properties, i, None) for i in prop_keys}
+    del_key = []
+    for item in dictionary:
+        prop = getattr(properties, item, None)
+        typ = type(prop)
+        if prop:
+            if typ == Vector:
+                # 属性阵列-浮点数组
+                dictionary[item] = dictionary[item].to_tuple()
+            elif typ == Euler:
+                dictionary[item] = dictionary[item][:]
+            elif typ == Matrix:
+                dictionary[item] = tuple(i[:] for i in dictionary[item])
+            elif typ == bpy.types.bpy_prop_array:
+                dictionary[item] = dictionary[item][:]
+            elif typ in (str, bool, float, int, set, list, tuple):
+                ...
+            elif typ.__name__ in [
+                'TRANSFORM_OT_shrink_fatten',
+                'TRANSFORM_OT_translate',
+                'TRANSFORM_OT_edge_slide',
+                'NLA_OT_duplicate',
+                'ACTION_OT_duplicate',
+                'GRAPH_OT_duplicate',
+                'TRANSFORM_OT_translate',
+                'OBJECT_OT_duplicate',
+                'MESH_OT_loopcut',
+                'MESH_OT_rip_edge',
+                'MESH_OT_rip',
+                'MESH_OT_duplicate',
+                'MESH_OT_offset_edge_loops',
+                'MESH_OT_extrude_faces_indiv',
+            ]:  # 一些奇怪的操作符属性,不太好解析也用不上
+                ...
+                del_key.append(item)
+            else:
+                print('emm 未知属性,', typ, dictionary[item])
+                del_key.append(item)
+    for i in del_key:
+        dictionary.pop(i)
+    return dictionary
+
+
+def draw_keymap(layout):
+    from rna_keymap_ui import draw_kmi
+    from .key import keymaps
+
+    kc = bpy.context.window_manager.keyconfigs.user
+    for km, kmi in keymaps:
+        km = kc.keymaps.get(km.name)
+        if km:
+            kmi = km.keymap_items.get(kmi.idname, get_kmi_operator_properties(kmi))
+            if kmi:
+                draw_kmi(["USER", "ADDON", "DEFAULT"], kc, km, kmi, layout, 0)
 
 
 def register():
-    global kmi
-    kmi = km.keymap_items.new(
-        idname=HdrRotationOperator.bl_idname,
-        type="RIGHTMOUSE",
-        value="PRESS",
-        ctrl=False,
-        shift=True,
-        alt=False,
-    )
+    global keymaps
+    kc = bpy.context.window_manager.keyconfigs.addon  # 获取按键配置addon的
+    km = kc.keymaps.new(name='3D View', space_type='VIEW_3D', region_type='WINDOW')
+    kmi = km.keymap_items.new(idname='wm.call_menu_pie',
+                              type="RIGHTMOUSE",
+                              value='PRESS',
+                              ctrl=False,
+                              shift=True,
+                              alt=False,
+                              )
+    kmi.show_expanded = True
+
+    keymaps.append((km, kmi))
 
 
 def unregister():
-    km.keymap_items.remove(kmi)
+    for km, kmi in keymaps:
+        km.keymap_items.remove(kmi)
+    keymaps.clear()
